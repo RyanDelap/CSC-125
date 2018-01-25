93cbe917d371ee4a7922b928ea31898e97a63d0a
RyanDelap <akayungeagle@gmail.com>
2018-01-25 13:11:33 -0600

    Add source files for practice 1 and 2

---
+++ practice2/smiley.cpp
@@ -0,0 +1,16 @@
+//Programmer: Ryan Delap
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+
+	cout << "   __\\_|_/__\n";
+	cout << "  /         \\ \n";
+	cout << " /  O    O   \\ \n";
+	cout << "(|    --     |) \n";
+	cout << " \\   \\__/    / \n";
+	cout << "  \\_________/ \n";
+
+}
